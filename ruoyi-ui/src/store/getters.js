0c50ad2958b5d26ded8fdfc293b1b4b2a3dfd93a
Will_Feng <u3breeze@gmail.com>
2023-09-28 23:33:21 +0800

    去掉token的重复存储，统一使用sa-token管理的token

--- ruoyi-ui/src/store/getters.js
+++ ruoyi-ui/src/store/getters.js
@@ -4,7 +4,6 @@ const getters = {
   device: state => state.app.device,
   visitedViews: state => state.tagsView.visitedViews,
   cachedViews: state => state.tagsView.cachedViews,
-  token: state => state.user.token,
   avatar: state => state.user.avatar,
   name: state => state.user.name,
   introduction: state => state.user.introduction,
