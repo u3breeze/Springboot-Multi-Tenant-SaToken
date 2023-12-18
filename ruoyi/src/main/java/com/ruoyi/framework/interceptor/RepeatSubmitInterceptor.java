7c8cb29568872c74d4b71cf772c4ae80a82e85f9
Will_Feng <u3breeze@gmail.com>
2023-12-18 14:50:42 +0800

    The HandlerInterceptorAdapter class is deprecated as of Spring 5.3. RepeatSubmitInterceptor implements HandlerInterceptor instead of extending HandlerInterceptorAdapter.

--- ruoyi/src/main/java/com/ruoyi/framework/interceptor/RepeatSubmitInterceptor.java
+++ ruoyi/src/main/java/com/ruoyi/framework/interceptor/RepeatSubmitInterceptor.java
@@ -3,13 +3,15 @@ package com.ruoyi.framework.interceptor;
 import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import com.alibaba.fastjson.JSONObject;
+
+import com.alibaba.fastjson.JSON;
 import com.ruoyi.common.utils.ServletUtils;
 import com.ruoyi.framework.interceptor.annotation.RepeatSubmit;
 import com.ruoyi.framework.web.domain.AjaxResult;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
  * 防止重复提交拦截器
@@ -17,39 +19,38 @@ import com.ruoyi.framework.web.domain.AjaxResult;
  * @author ruoyi
  */
 @Component
-public abstract class RepeatSubmitInterceptor extends HandlerInterceptorAdapter
+public abstract class RepeatSubmitInterceptor implements HandlerInterceptor
 {
+    /**
+     * 防止重复提交拦截器
+     * @param request http请求
+     * @param response http响应
+     * @param handler 处理器
+     * @return 是否通过
+     */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
+    public boolean preHandle(@Nullable HttpServletRequest request, @Nullable  HttpServletResponse response, @Nullable Object handler)
     {
         if (handler instanceof HandlerMethod)
         {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             Method method = handlerMethod.getMethod();
             RepeatSubmit annotation = method.getAnnotation(RepeatSubmit.class);
-            if (annotation != null)
+            if (annotation != null && (this.isRepeatSubmit(request) && response != null))
             {
-                if (this.isRepeatSubmit(request))
-                {
-                    AjaxResult ajaxResult = AjaxResult.error("不允许重复提交，请稍后再试");
-                    ServletUtils.renderString(response, JSONObject.toJSONString(ajaxResult));
-                    return false;
-                }
+                AjaxResult ajaxResult = AjaxResult.error("不允许重复提交，请稍后再试");
+                ServletUtils.renderString(response, JSON.toJSONString(ajaxResult));
+                return false;
             }
-            return true;
         }
-        else
-        {
-            return super.preHandle(request, response, handler);
-        }
+        return true;
     }
 
     /**
      * 验证是否重复提交由子类实现具体的防重复提交的规则
-     * 
-     * @param httpServletRequest
-     * @return
-     * @throws Exception
+     *
+     * @param  request http请求
+     * @return 是否重复提交
      */
     public abstract boolean isRepeatSubmit(HttpServletRequest request);
 }
